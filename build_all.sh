0e404cb37aa4627fb9cf9ae8c2a23de5242e101c
Joe VLcek <jvlcek@redhat.com>
2018-05-03 16:49:51 -0400

    Temp changes needed to build against jvlcek repo

--- build_all.sh
+++ build_all.sh
@@ -1,15 +1,145 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-pushd images
+error_exit() {
+  echo -e "\nERROR EXITING: ${1}"
+  exit 1
+}
+
+run_cmd () {
+  run_cmd_status=0
+
+  echo -e "  $@"
+  if [[ $trial_run == false ]]; then
+    eval "$@"
+    run_cmd_status=$?
+  fi
+}
+
+usage() {
+  this_script="$(basename $0)"
+
+  # Optional
+  echo "Usage: ${this_script} [options]"
+
+  echo "Optional options:"
+  echo "  -t                        [false]    trial_run flag \\"
+  echo "  -r                        [manageiq] registry name \\"
+
+  echo "Example usage:"
+  echo "${this_script} \\"
+  echo "  -r my_registry \\"
+  echo ""
+}
+
+parse_args() {
+  echo -e "\n*** Invoked function ${FUNCNAME} ***"
+
+  registry="manageiq"
+  trial_run=false
+
+  while getopts "r:t" opt; do
+    case $opt in
+    r)
+      registry=$OPTARG
+      ;;
+    t)
+      trial_run=true
+      ;;
+    ?)
+      echo "*** ? ***"
+      usage
+      exit 1
+      ;;
+    :)
+      echo "*** : ***"
+      usage
+      exit 1
+      ;;
+    esac
+  done
+
+  echo -e "\n    trial_run: ${trial_run}"
+  echo -e "     registry: ${registry}"
+}
+
+cwd_images() {
+  echo -e "\n*** Invoked function ${FUNCNAME} ***"
+
+  if [[ -d images ]]; then
+    pushd images
+  else
+    error_exit "images directory not found"
+  fi
+}
+
+rm_old_logs() {
+  echo -e "\n*** Invoked function ${FUNCNAME} ***"
+
   find . -name build.log | xargs rm
+}
 
-  docker build manageiq-base -t manageiq/manageiq-base:latest --no-cache > manageiq-base/build.log
+docker_cmd() {
+  if [[ $# -ne 2 ]]; then
+    error_exit "${FUNCNAME} invoked with wrong number of arguments"
+  fi
 
-  docker build manageiq-orchestrator -t manageiq/manageiq-orchestrator:latest > manageiq-orchestrator/build.log && docker push manageiq/manageiq-orchestrator:latest >> manageiq-orchestrator/build.log &
+  sub_cmd="$1"
+  image_name="$2"
 
-  docker build manageiq-base-worker -t manageiq/manageiq-base-worker:latest > manageiq-base-worker/build.log && docker push manageiq/manageiq-base-worker:latest >> manageiq-base-worker/build.log
+  if ! [[ -d ${image_name} ]]; then
+    error_exit "${image_name} directory not found"
+  fi
 
-  docker build manageiq-webserver-worker -t manageiq/manageiq-webserver-worker:latest > manageiq-webserver-worker/build.log && docker push manageiq/manageiq-webserver-worker:latest >> manageiq-webserver-worker/build.log
+  if [[ ${sub_cmd} == "build" ]]; then
+    run_cmd "docker build ${image_name} -t ${registry}/${image_name}:latest --no-cache > ${image_name}/build.log"
+  elif [[ ${sub_cmd} == "push" ]]; then
+    run_cmd "docker push ${registry}/${image_name}:latest >> ${image_name}/build.log"
+  else
+    error_exit "Unsupported docker sub command ${sub_cmd}"
+  fi
+}
 
-  docker build manageiq-ui-worker -t manageiq/manageiq-ui-worker:latest > manageiq-ui-worker/build.log && docker push manageiq/manageiq-ui-worker:latest >> manageiq-ui-worker/build.log
+
+docker_build_all_images() {
+  echo -e "\n*** Invoked function ${FUNCNAME} ***"
+
+  docker_cmd "build" "manageiq-base"
+  docker_cmd "build" "manageiq-orchestrator"
+  docker_cmd "build" "manageiq-base-worker"
+  docker_cmd "build" "manageiq-webserver-worker"
+  docker_cmd "build" "manageiq-ui-worker"
+}
+
+docker_push_all_images() {
+  echo -e "\n*** Invoked function ${FUNCNAME} ***"
+
+  docker_cmd "push" "manageiq-base"
+  docker_cmd "push" "manageiq-orchestrator"
+  docker_cmd "push" "manageiq-base-worker"
+  docker_cmd "push" "manageiq-webserver-worker"
+  docker_cmd "push" "manageiq-ui-worker"
+}
+
+echo -e "\n*** In main***"
+
+parse_args "$@"
+cwd_images
+# docker_build_all_images
+# docker_push_all_images
+
+docker_cmd "build" "manageiq-base"
+docker_cmd "push" "manageiq-base"
+
+docker_cmd "build" "manageiq-orchestrator"
+docker_cmd "push" "manageiq-orchestrator"
+
+docker_cmd "build" "manageiq-base-worker"
+docker_cmd "push" "manageiq-base-worker"
+
+docker_cmd "build" "manageiq-webserver-worker"
+docker_cmd "push" "manageiq-webserver-worker"
+
+docker_cmd "build" "manageiq-ui-worker"
+docker_cmd "push" "manageiq-ui-worker"
+
 popd
